e1f8002de6af255a3918f20354d6605a1a239d8c
johnyalfred <sjohnyalfred@gmail.com>
2015-12-09 05:24:44 +0530

    added.

---
+++ Session201/app/src/main/java/com/app/appnameicon/session201/MainActivity.java
@@ -0,0 +1,13 @@
+package com.app.appnameicon.session201;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
