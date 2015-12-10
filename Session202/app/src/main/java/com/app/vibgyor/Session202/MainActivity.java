8ce8c1537cb9577cefcd9711cceb1ef3d58ccac7
johnyalfred <sjohnyalfred@gmail.com>
2015-12-10 18:55:36 +0530

    added.

---
+++ Session202/app/src/main/java/com/app/vibgyor/Session202/MainActivity.java
@@ -0,0 +1,15 @@
+package com.app.vibgyor.Session202;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.vibgyor.Session202.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
